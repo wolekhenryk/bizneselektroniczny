f2e8c4d769b6226dc9a1dea729f320cc1be567a7
Henryk Wołek <henrykwolek15@gmail.com>
2024-10-20 21:44:45 +0200

    Rekurencyjny scraping kategorii, serializacja kategorii do pliku .json oraz przechowywanie linków do kategorii

--- BiznesElektroniczny_scraper/BiznesElektroniczny_scraper.API/Models/Category.cs
+++ BiznesElektroniczny_scraper/BiznesElektroniczny_scraper.API/Models/Category.cs
@@ -1,7 +1,15 @@
 ﻿namespace BiznesElektroniczny_scraper.API.Models;
 
-public class Category {
-    public string Name { get; set; } = "No category";
-    public string Description { get; set; } = "No description";
-    public SortedSet<Category> Subcategories { get; set; } = [];
+public class Category(string categoryName = "No category") : IComparable<Category> {
+    public string Name { get; set; } = categoryName;
+    public string Url { get; set; } = string.Empty;
+    public ISet<Category> Subcategories { get; set; } = new SortedSet<Category>();
+
+    public int CompareTo(Category? other) {
+        if (ReferenceEquals(this, other)) return 0; // Both objects are the same
+        return ReferenceEquals(null, other)
+            ? 1
+            : // Other object is null
+            string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+    }
 }
--- BiznesElektroniczny_scraper/BiznesElektroniczny_scraper.API/Services/Background/ScrapingHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using BiznesElektroniczny_scraper.API.Services.Scraping;
-
-namespace BiznesElektroniczny_scraper.API.Services.Background;
-
-public class ScrapingHandler(ScrapingService scrapingService) : BackgroundService {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken) => await scrapingService.ScrapeAsync();
-}
